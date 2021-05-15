e98c46f16bf6afd1ac13765dadda6c423e375f2d
albert hdez <albertohtesta@gmail.com>
2021-05-15 12:19:54 -0700

    Add flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
 	def index
+		#flash.now[:notice] = "Logged succesfully" para solo ese request
+		flash[:notice] = "Logged succesfully"
+		flash[:alert] = "Invalid password"
 	end
 end
