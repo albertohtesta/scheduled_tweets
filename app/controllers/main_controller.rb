715c18b2239d9a06ae70bf8b4b39572f8344d88b
albert hdez <albertohtesta@gmail.com>
2021-06-19 11:19:31 -0700

    initial commit

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,13 @@
 class MainController < ApplicationController
 	def index
+
+	#if session[:user_id]   esto se paso a application_controller
+		#@user = User.find(session[:user_id]) marca error si el user no existe en la BD
+		@user = User.find_by(id: session[:user_id])
+	#end
+
 		#flash.now[:notice] = "Logged succesfully" para solo ese request
-		flash[:notice] = "Logged succesfully"
-		flash[:alert] = "Invalid password"
+		#flash[:notice] = "Logged succesfully"
+		#flash[:alert] = "Invalid password"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,33 @@
 Rails.application.routes.draw do
 	get "about-us", to: "about#index", as: :about
+	
+	# cuando se olvida el password - pantalla donde se da el email y controler que mande email
+	get "password/reset", to: "password_resets#new"
+	post "password/reset", to: "password_resets#create"
+	
+	# pantalla cuando se hace le cambio de email
+	get "password/reset/edit", to: "password_resets#edit"
+	patch "password/reset/edit", to: "password_resets#update"
+
+	
+	get "password", to: "passwords#edit", as: :edit_password
+	patch "password", to: "passwords#update"
+
+
+	get "sign_up", to: "registrations#new"
+	post "sign_up", to: "registrations#create"
+
+	get "sign_in", to: "sessions#new"
+	post "sign_in", to: "sessions#create"
+
+	# esta es la callback url que indicamos en twitter
+	get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
+
+	# aqui se crean as rutas genericas de twitter_accounts
+	resources :twitter_accounts
+	resources :tweets
+
+	delete "logout", to: "sessions#destroy"
 	# get "/", to: "main#index" o tambien: get "" o:
 	root to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
