249a1fda4c0c7ebb95fcbe0c18df049c62744077
albert hdez <albertohtesta@gmail.com>
2025-11-05 14:47:53 -0500

    twitter start

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_04_134744) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_05_183313) do
+  create_table "twitter_accounts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "image"
+    t.string "name"
+    t.string "secret"
+    t.string "token"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.string "username"
+    t.index ["user_id"], name: "index_twitter_accounts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
     t.string "password_digest"
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "twitter_accounts", "users"
 end
